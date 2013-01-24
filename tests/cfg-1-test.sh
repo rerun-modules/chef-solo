79488ba2ec60f38ac7a6e54a85463759ccc881ff
Lee Thompson <stagr.lee@gmail.com>
2013-01-24 16:41:06 -0600

    start of app provisioning lifecycle

---
+++ tests/cfg-1-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the `cfg` command.
+#    
+#/ usage:  rerun stubbs:test -m chef-solo -p cfg [--answers <>]
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "cfg"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
---
+++ tests/install-1-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the `install` command.
+#    
+#/ usage:  rerun stubbs:test -m chef-solo -p install [--answers <>]
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "install"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
